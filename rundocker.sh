2c75fefeb4c4660da4fb5a4fbdb7330129331dd8
Soichi Hayashi <hayashis@iu.edu>
2017-06-23 23:07:06 +0000

    updated REAMDE

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,13 @@
-docker run -it \
-    -v /Users/lindseykitchell/Documents/Github_repos/app-splitshells/input:/input \
+
+#mkdir input
+scp -r karst.uits.iu.edu:/N/u/hayashis/Karst/testdata/app-splitshells/* input/
+
+#mkdir output
+#cp config.json output
+
+docker run -it --rm \
+    -v `pwd`/testdata:/input \
     -v `pwd`:/output \
     brainlife/splitshells
+
+#-v /Users/lindseykitchell/Documents/Github_repos/app-splitshells/input:/input \

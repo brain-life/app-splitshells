4b789c628264331f6bbf9703a60d8f2e90b1636c
Soichi Hayashi <hayashis@iu.edu>
2018-08-09 16:55:08 -0400

    finished initial merging of datanormalize

--- main.m
+++ main.m
@@ -10,7 +10,6 @@ end
 config = loadjson('config.json');
 
 % Parameters used for normalization
-params.shells       = config.shell;
 bvals = dlmread(config.bvals);
 bvecs = dlmread(config.bvecs);
 dwi = niftiRead(config.dwi);
@@ -19,22 +18,18 @@ dwi = niftiRead(config.dwi);
 [bvals_unique, ~, bvals_uindex] = unique(bvals);
 bvals_unique(bvals_unique <= config.b0_max) = 0;
 bvals_unique = round(bvals_unique./config.bvals_round)*config.bvals_round;
-bvals_round = bvals_unique( bvals_uindex )
+bvals_round = bvals_unique( bvals_uindex );
 
-for i = 1:length(params.shells)
-    index = (bvals_round == params.shells(i));
-    index0 = (bvals_round == 0);
-    all_index = or(index, index0);
-    assertEqual(sum(all_index), sum(index0) + sum(index));
-    
-    %write files
-    dlmwrite(sprintf('dwi.bvals', params.shells(i)), bvals_round(all_index), 'delimiter',' ');
-    dlmwrite(sprintf('dwi.bvecs', params.shells(i)), bvecs(:,all_index));
-    dwi_oneshell = dwi;
-    dwi_oneshell.fname = sprintf('dwi.nii.gz', params.shells(i));
-    dwi_oneshell.data = dwi.data(:,:,:,all_index);
-    dwi_oneshell.dim(4) = size(dwi_oneshell.data,4);
-    niftiWrite(dwi_oneshell);
-end
+index = (bvals_round == config.shell);
+index0 = (bvals_round == 0);
+all_index = or(index, index0);
+assertEqual(sum(all_index), sum(index0) + sum(index));
 
-
+%write files
+dlmwrite('dwi.bvals', bvals_round(all_index), 'delimiter',' ');
+dlmwrite('dwi.bvecs', bvecs(:,all_index), 'delimiter', ' ');
+dwi_oneshell = dwi;
+dwi_oneshell.fname = 'dwi.nii.gz';
+dwi_oneshell.data = dwi.data(:,:,:,all_index);
+dwi_oneshell.dim(4) = size(dwi_oneshell.data,4);
+niftiWrite(dwi_oneshell);
